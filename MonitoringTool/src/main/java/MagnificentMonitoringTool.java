2f04351e627b05832d052cd1892b098dfc208a4d
Tim Junge <Timboboy123@googlemail.com>
2021-12-09 22:13:57 +0100

    Update gitignore file, update readme, enable properties file configuration for logging I decided that it's preferable to configure the logger via a properties file and thus replaced the old logger initialization with the new properties file.

--- MonitoringTool/src/main/java/MagnificentMonitoringTool.java
+++ MonitoringTool/src/main/java/MagnificentMonitoringTool.java
@@ -1,27 +1,19 @@
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class MagnificentMonitoringTool {
     public static void main(String[] args) {
-        FileHandler fileHandler = null;
+        if(args.length == 1 && args[0].equals("help") || args.length != 4) {
+            System.out.println("MonitoringTool - Required parameters: URL INTERVAL TIMEOUT PATH/TO/PROPERTIES/FILE/FOR/LOGGER/");
+            return;
+        }
+
         try {
-            if(args.length == 1 && args[0].equals("help") || args.length != 4) {
-                System.out.println("MonitoringTool - Required parameters: URL INTERVAL TIMEOUT PATH/TO/DIRECTORY/FOR/LOGFILES/");
-                return;
-            }
-
-            /* I could have used a properties file to configure the logger, but I decided against since I wanted to accept the log file path as parameter*/
-            var logger = Logger.getLogger("MagnificentMonitoringTool");
-            var formatter = DateTimeFormatter.ofPattern("yyy-MM-dd-HH-mm-ss");
-            fileHandler = new FileHandler(args[3] + formatter.format(LocalDateTime.now()) + "_logfile.log");
-            logger.addHandler(fileHandler);
-            fileHandler.setFormatter(new SimpleFormatter());
-
+            LogManager.getLogManager().readConfiguration(new FileInputStream(args[3]));
+            var logger = Logger.getLogger(MagnificentMonitoringTool.class.getName());
             var tester = new ConnectionTester(new URL(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), logger);
 
             while (true) {
@@ -30,10 +22,6 @@ public class MagnificentMonitoringTool {
             }
         } catch(IOException error) {
             error.printStackTrace();
-        } finally {
-            if (fileHandler != null) {
-                fileHandler.close();
-            }
         }
     }
 }
--- README.md
+++ README.md
@@ -13,7 +13,14 @@ Executing the tests requires mockito. <br />
 The project was setup with maven.
 
 ### Executing program
-java -jar MonitoringTool.jar URL TESTINGINTERVAL TIMEOUT PATH/TO/TARGETFOLDER/ <br />
+java -jar MonitoringTool.jar URL TESTINGINTERVAL TIMEOUT PATH/TO/PROPERTIES/FILE/FOR/LOGGER/<br />
 java -jar MonitoringTool.jar help to show the parameters required by the tool
+
+### Example logger config
+handlers = java.util.logging.FileHandler, java.util.logging.ConsoleHandler<br />
+java.util.logging.FileHandler.pattern = C:/temp/logfile%g.log<br />
+java.util.logging.FileHandler.formatter = java.util.logging.SimpleFormatter<br />
+java.util.logging.FileHandler.limit = 1000000 <br />
+java.util.logging.FileHandler.level = ALL
 
 
